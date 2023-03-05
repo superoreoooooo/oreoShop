d1ee08665b376042271f711c202d6b67e1baddfc
oreo <oooowwww365@gmail.com>
2023-03-05 12:40:18 +0900

    0305

--- src/main/java/win/oreo/shop/Shop.java
+++ src/main/java/win/oreo/shop/Shop.java
@@ -1,17 +1,15 @@
 package win.oreo.shop;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import win.oreo.inventory.Inventory.oreoInventory;
 
 public final class Shop extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
