a6a985393e43587c8ae574b461675f5265fd3b3c
oreo <oooowwww365@gmail.com>
2023-03-05 12:33:57 +0900

    Initial commit

---
+++ src/main/java/win/oreo/shop/Shop.java
@@ -0,0 +1,17 @@
+package win.oreo.shop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Shop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
